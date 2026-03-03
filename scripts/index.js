0eed55a523b85733fff52b59a5b14519bb1ec041
Arthur-thecreator <arthur.mendes@aluno.ifsp.edu.br>
2026-03-03 15:50:36 -0300

    Inclusão da parte de js

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+import { cityWeather } from "./data.js";
+
+
+
+function renderBannerInfo(data){
+
+}
+
+function renderDayInfo(data){
+
+}
+
+function renderDaily(dailyData){
+
+}
+
+function renderHourly(hourlyData){
+
+}
+
+
+function orchestrerFunction(){
+    renderBannerInfo()
+    renderDayInfo()
+    renderDaily()
+    renderHourly()
+}
+
+
+orchestrerFunction();
